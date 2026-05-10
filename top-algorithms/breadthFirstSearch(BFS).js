0d4894902681389d2463582b4f336b37dd696e6b
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2026-05-10 19:17:45 +0545

    Search Algorithms: BFS Traversal

---
+++ top-algorithms/breadthFirstSearch(BFS).js
@@ -0,0 +1,29 @@
+const bfs = (graph, start) => {
+  const visited = new Set();
+  const queue = [start];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+
+    if (!visited.has(node)) {
+      console.log(node);
+      visited.add(node);
+
+      const neighbors = graph[node];
+      for (const neighbor of neighbors) {
+        queue.push(neighbor);
+      }
+    }
+  }
+};
+
+const graph = {
+  A: ["B", "C"],
+  B: ["A", "D", "E"],
+  C: ["A", "F"],
+  D: ["B"],
+  E: ["B", "F"],
+  F: ["C", "E"]
+};
+
+bfs(graph, "A");
